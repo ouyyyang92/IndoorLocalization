45ff7d1a0c94ecd36653a0ea23be97c9b73f028d
吴宗麟 <1160980378@qq.com>
2020-07-10 14:17:36 +0800

    调整了person 登录注册结束 定位制作完成 个人信息显示完成 2020.7.8 21:40代码格式化 消去不必要的注释 2020.7.8 23:28 加入退出登录 注册后可跳转 验证码错误有待修正 2020.7.9 12:34 加入第二个手机 调试头像 2020.7.10 14:17 备份

--- app/src/main/java/com/example/myapplication/ui/main/map/MapFragment.java
+++ app/src/main/java/com/example/myapplication/ui/main/map/MapFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,16 +43,18 @@ public class MapFragment extends Fragment {
     private ImageView iv_location;
     private float currentX = 0;
     private float currentY = 0;
-    private float newX,newY;
+    private float newX, newY;
     private MainActivity parent;
     private Button button_dingwei;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     private int[] AP = new int[6];
-    public float px,py;
+    public float px, py;
     private Timer mTimer;   // 启动定时任务的对象
     private final int SAMPLE_RATE = 2000; // 采样周期，以毫秒为单位，两秒一次
+    private String str1;
+    private WifiManager wm;
 
     public MapFragment() {
         // Required empty public constructor
@@ -90,33 +94,27 @@ public class MapFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_map, container, false);
 
     }
-    public void InitLocation(){
 
-        TranslateAnimation translateAnimation = new TranslateAnimation(currentX,newX,currentY,newY);
-                    translateAnimation.setDuration(100);
-                    iv_location.setAnimation(translateAnimation);
-                    translateAnimation.start();
+    public void InitLocation() {
+
+        TranslateAnimation translateAnimation = new TranslateAnimation(currentX, newX, currentY, newY);
+        translateAnimation.setDuration(100);
+        iv_location.setAnimation(translateAnimation);
+        translateAnimation.start();
     }
+
     public void init() {                 //数据初始化
         for (int a = 0; a < 6; a++) {
             AP[a] = -100;
         }
     }
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        parent= (MainActivity) getActivity();
-        newX=px;
-        newY=py;
-       iv_location= getActivity().findViewById(R.id.imageView_location);
-        button_dingwei = getActivity().findViewById(R.id.button);
-        button_dingwei.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1){
                 init();
-                WifiManager wm = (WifiManager) parent.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                wm.startScan();                                  //开始扫描AP
-                int j =0;
+
+                int j = 0;
                 List<ScanResult> results = wm.getScanResults();  //拿到扫描的结果
                 for (ScanResult result : results) {
                     if (result.SSID.equals("1604") && result.level > -100) {
@@ -129,39 +127,70 @@ public class MapFragment extends Fragment {
                     if (result.SSID.equals("1601T") && result.level > -100) {
                         AP[3] = result.level;
                     }
+                    if (result.SSID.equals("llalala") && result.level > -100) {
+                        AP[4] = result.level;
+                    }
+                    if (result.SSID.equals("HUAWEI-YJ520Q") && result.level > -100) {
+                        AP[5] = result.level;
+                    }
                 }
-                String str1 = "11";
-                for (int i = 0;i < 6;i++){
-                    str1 = str1 + " " + AP[i] ;
-                }
-                Client.send(str1);
+                str1 = "3 " + AP[0] + " " + AP[1] + " " + AP[2] + " " + AP[3] + " " + AP[4] + " " + AP[5];
+                String str2 = Client.send(str1);
+                String[] strings4 = str2.split("/");
+                String[] strings5 = strings4[1].split(" ");
+                px = (float) (Integer.parseInt(strings5[0]) * 410 / 12.0 / 6.8 * 16.5);
+                py = (float) ((20 - Integer.parseInt(strings5[1])) * 410 / 12 / 6.8 * 16.5);
 
-//                String str2 = Client.send(str1);
-//                String[] strings4 = str2.split("/");
-//                String[] strings5 = strings4[1].split(" ");
-//                px = (float)(Integer.parseInt(strings5[0])*410/12.0/6.8*16.5);
-//                py = (float)((20-Integer.parseInt(strings5[1]))*410/12/6.8*16.5);
             }
+            super.handleMessage(msg);
+        }
+    };
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        parent = (MainActivity) getActivity();
+        newX = px;
+        newY = py;
+        iv_location = getActivity().findViewById(R.id.imageView_location);
+        button_dingwei = getActivity().findViewById(R.id.button);
+        button_dingwei.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                mTimer = new Timer();
+                mTimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        wm = (WifiManager) parent.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                        wm.startScan();                                  //开始扫描AP
+                        Message message = new Message();
+                        message.what = 1;
+                        handler.sendMessage(message);
+
+                    }
+                }, 0, 15000); // 立即执行任务，每隔5000ms执行一次WiFi扫描的任务
+            }
+
+
         });
 
         new Thread(new Runnable() {
             @Override
             public void run() {
 //                int num = 0;
-                while(true){
+                while (true) {
 //                    num++;
 //                    newX = currentX + (100 - num) / 10;
 //                    newY = currentY - (100 - num) / 10;
-                    newX=px;
-                    newY=py;
-                    TranslateAnimation translateAnimation = new TranslateAnimation(currentX,newX,currentY,newY);
-                    translateAnimation.setDuration(2000);
+                    newX = px;
+                    newY = py;
+                    TranslateAnimation translateAnimation = new TranslateAnimation(currentX, newX, currentY, newY);
+                    translateAnimation.setDuration(100);//动画持续时间
                     iv_location.setAnimation(translateAnimation);
                     translateAnimation.start();
                     currentX = newX;
                     currentY = newY;
                     try {
-                        Thread.sleep(100);
+                        Thread.sleep(100);//线程持续时间
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
